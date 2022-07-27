f21a1890b9080dd749dc33d95566b094833fc270
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-07-28 04:34:47 +0900

    Update Introduction-to-Nim-Game.cpp

--- Introduction-to-Nim-Game.cpp
+++ Introduction-to-Nim-Game.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'nimGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY pile as parameter.
+ */
+
+string nimGame(vector<int> pile) {
+
+}
 
